f79442a00e752d63c1efe4949467656f7f7ec958
paulo-web-dev <paulosergioorfanelli@gmail.com>
2025-12-16 18:50:06 -0300

    f

--- app/Http/Controllers/EmpresaController.php
+++ app/Http/Controllers/EmpresaController.php
@@ -528,7 +528,7 @@ class EmpresaController extends Controller
             'totalParticipantesGeral' => $totalParticipantesGeral,
             'mediasPorSetor' => $mediasPorSetor,
         ]); 
-    } 
+    }     
 
     
     public function imprimedashboardarp($id){
